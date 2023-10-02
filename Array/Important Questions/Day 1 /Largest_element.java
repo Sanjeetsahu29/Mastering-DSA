993c138e926fa2a4329b32d053fb7f878177b17c
Sanjeet kumar sahu <108270460+Sanjeetsahu29@users.noreply.github.com>
2023-10-02 18:28:50 +0530

    Update Largest_element.java

--- Array/Important Questions/Day 1 /Largest_element.java
+++ Array/Important Questions/Day 1 /Largest_element.java
@@ -2,7 +2,7 @@ public class Largest_element {
     public static void main(String[] args) {
         int[] arr={2,1,4,5,6,7,1,23,1,4,4,4,6,6,7,12,13};
         int largest=arr[0]; //here we have considered the first element of the array to be the largest element
-        for (int i = 1; i < arr.length; i++) { //why i starting from 1 not 0, (Ans-First element is assumed to be the largest element so need to do comparison)
+        for (int i = 1; i < arr.length; i++) { //why I starting from 1 not 0, (Ans-First element is assumed to be the largest element so need to do a comparison)
             if(arr[i]>largest) largest=arr[i];
         }
         System.out.println("Largest element of the array is "+largest);
